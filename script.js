d4b5bc92d129614e64f5a5e3fc7c8148381ec23e
kemiwealth <kjcartel0@gmail.com>
2025-09-26 16:04:22 -0700

    working on .js

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
+const taskNameInput = document.getElementById('task-name');
+const categoryInput = document.getElementById('category');
+const deadlineInput = document.getElementById('deadline');
+const statusInput = document.getElementById('status');
+const addTaskBtn = document.getElementById('add-task-btn');
+const taskListUl = document.getElementById('task-list');
+
+const filterStatus = document.getElementById("filter-status");
+const filterCategory = document.getElementById("filter-category");
+
+let tasks = [];
+
+function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function loadTasks() {
+    const storedTasks = JSON.parse(localStorage.getItem("tasks"));
+    if (storedTasks) {
+        tasks = storedTasks; 
+    }
+    displayTasks();
+}
+ 
+function addTask() {
+    const taskName = taskNameInput.value;
+    const category = categoryInput.value;
+    const deadline = deadlineInput.value;
+    const status = statusInput.value;
+
+    if (!taskName || !category || !deadline || !status) {
+        alert("Please fill in all fields.");
+        return;
+    }
+
+    const newTask = { taskName, category, deadline, status };
+
+    tasks.push(newTask);
+    saveTasks();   
+    displayTasks();
+
+    taskNameInput.value = "";
+    categoryInput.value = "Categories";
+    deadlineInput.value = "";
+    statusInput.value = "Status";
+}
 
